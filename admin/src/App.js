565e9e2b1806c0e3dec12bffd5407201e3d598a4
jefbar1974 <jefbar@gmail.com>
2023-02-14 22:10:49 -0300

    Aula2

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,9 +1,38 @@
 import React from 'react'
+import { 
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import Menu from './components/Menu/index.js'
+import Home from './pages/Home/index.js'
 
 
-function App() {
+const App = ()  => {
   return (
-    <h1>React App</h1>
+    <>
+      <BrowserRouter>
+        <Menu/>
+        <div className='container mt-3'>
+          <Routes>
+            <Route path='/'>
+              <Route index element={<Home/>}/>
+
+              <Route path='departamentos'>
+                <Route index element={<h1>Listagem de Deparetamentos</h1>}/>
+                <Route path='new' element={<h1>Cadastra Deparetamentos</h1>}/>
+                <Route path=':idDepartamento' element={<h1>Editar Deparetamentos</h1>}/>
+              </Route>
+            </Route>
+          </Routes>
+        </div>
+
+      </BrowserRouter>
+    </>
   )
 }
 
---
+++ admin/src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <h3 className="mt-3">Bem Vindo ao Admin</h3>
+  )
+}
+
+export default Home
